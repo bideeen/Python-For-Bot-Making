cb9a8d4e5a927b16b2dbe505dde05f2dd666c09d
bideeen <b.abideen@yahoo.com>
2021-01-21 04:08:53 -0700

    alpha.1.1

--- Module 1 - Django/Applications and Routes/app/blog/views.py
+++ Module 1 - Django/Applications and Routes/app/blog/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    """
+    Homepage
+    """
+    return HttpResponse('<h1>Blog Home </h1>')
 
+def about(request):
+    """
+    about page
+    """
+    return HttpResponse('<h1>Blog About </h1>')
